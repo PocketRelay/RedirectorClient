b4f0b1be0be0fc481d28e03b2c66913065a85a09
Jacobtread <jacobtread@gmail.com>
2022-10-13 00:10:03 +1300

    Ported to blazessl and working!

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 
 This is a working version of the **Pocket Relay** new system where the redirector server
 is hosted on the individual client computers rather than on an external server.
-
-Note this is unfinished at the moment it requires SSLv3 to be enabled through a registry 
-tweak and the following ciphers need to be enabled:
-- TLS_RSA_WITH_RC4_128_MD5 
-- TLS_RSA_WITH_RC4_128_SHA 
--- src/server.rs
+++ src/server.rs
@@ -1,41 +1,24 @@
 use std::{io, thread};
+use std::io::{Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, PoisonError, RwLock};
 use blaze_pk::{group, OpaquePacket, packet, PacketError, Packets, TdfOptional};
-use blaze_schannel::{CertStore, SchannelCred, TlsStream};
+use blaze_ssl::stream::SslStream;
 use crate::shared::SharedState;
+
 
 /// Starts the server
 pub fn run_server(state: Arc<RwLock<SharedState>>) {
-    // Import redirector store
-    let cert_store = {
-        let store_bytes = include_bytes!("redirector.pfx");
-        let store_password = "123456";
-
-        CertStore::import_pkcs12(store_bytes, Some(store_password))
-            .expect("Failed to import store")
-    };
-
-    // Load certificate from store
-    let cert = cert_store
-        .first()
-        .expect("Store missing certificate");
-
-    // Create schannel credentials
-    let cred = SchannelCred::new(cert.clone())
-        .expect("Failed to create schannel cred");
-
     // Begin listening for connections
     let listener = TcpListener::bind(("0.0.0.0", 42127))
         .expect("Failed to bind TCP listener");
 
     for stream in listener.incoming() {
-        let cred = cred.clone();
         let state = state.clone();
         thread::spawn(move || {
             let stream = stream.expect("Failed to accept stream");
-            let stream = &mut TlsStream::new(cred, stream)
-                .expect("Failed to complete handshake");
+            let stream =
+                &mut SslStream::new(stream).expect("Failed to complete handshake");
             let _ = handle_client(stream, state);
         });
     }
@@ -82,10 +65,13 @@ group! {
 }
 
 pub fn handle_client(
-    stream: &mut TlsStream<TcpStream>,
+    stream: &mut SslStream<TcpStream>,
     state: Arc<RwLock<SharedState>>,
 ) -> Result<(), ClientError> {
     let packet = OpaquePacket::read(stream)?;
+
+    println!("{packet:?}");
+
     let state = state.read()?;
     let content = RedirectPacket {
         ADDR: TdfOptional::default_some("VALUE", AddressGroup {
@@ -98,6 +84,9 @@ pub fn handle_client(
 
     let response = Packets::response(&packet, content);
     response.write(stream)?;
-    stream.shutdown()?;
+
+
+    stream.flush()?;
+    // stream.shutdown()?;
     Ok(())
 }
